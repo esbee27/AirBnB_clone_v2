7519166e282447946bd220db2d11993b6e7f4a6f
Olaoye Ademola <demmydd44@gmail.com>
2024-06-25 13:49:56 -0700

    more fixes

--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-	__tablename__ = "name"
-	email = Column(String(128), nullable=False
-	password = Column(String(128), nullable=False)
-    	first_name = Column(String(128), nullable=True)
-    	last_name = Column(String(128), nullable=True)
+    __table_args__ = {'extend_existing': True}
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
